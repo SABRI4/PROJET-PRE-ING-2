af4cc361a0f2d08103cddc3432e8b34eb42bab58
Pachax17 <131616395+Pachax17@users.noreply.github.com>
2023-12-13 11:14:50 +0100

    V2 s1.sh

--- s1.sh
+++ s1.sh
@@ -19,3 +19,29 @@ elif [ "$1" = '-h' ] ; then
 echo "message d'aide"
 fi
 fi
+
+
+
+#!/bin/bash
+
+fichier_csv="data/data.csv"
+
+if [ -n "$1" ] ; then
+    echo "paramètre 1 existe"
+
+    if [ "$1" = '-D1' ] ; then
+        exec_timestart_D1=$(date +%s.%N)
+        echo "paramètre 1 = D1"
+        
+        # Manipulation des données en une seule commande
+        awk -F';' '{count[$1" "$6]++} END {for (word in count) print count[word], word}' "$fichier_csv" | sort -rn | head -n10 > "temp/D1_temp_list.txt"
+        
+        exec_timeend_D1=$(date +%s.%N) 
+        exec_timetotal_D1=$(echo "$exec_timeend_D1 - $exec_timestart_D1" | bc)
+        echo "temps d'execution : $exec_timetotal_D1"
+
+    elif [ "$1" = '-h' ] ; then
+        echo "message d'aide"
+    fi
+fi
+
