d528da2bee6126ef42b08f2e327cdc238d56b8a9
SABRI4 <131693312+SABRI4@users.noreply.github.com>
2023-12-18 17:41:40 +0100

    Update code1.c

--- code1.c
+++ code1.c
@@ -8,6 +8,7 @@
 struct Driver {
     char route_id[MAX_LINE_LENGTH];
     char name[MAX_LINE_LENGTH];
+    int unique_routes;
 };
 
 // AVL Tree Node
@@ -16,7 +17,6 @@ struct Node {
     struct Node *left;
     struct Node *right;
     int height;
-    int unique_routes;
 };
 
 // Function to get the height of a node
@@ -31,47 +31,6 @@ int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-// Function to get the balance factor of a node
-int getBalance(struct Node *node) {
-    if (node == NULL)
-        return 0;
-    return height(node->left) - height(node->right);
-}
-
-// Function to rotate a subtree rooted with y to the right
-struct Node *rightRotate(struct Node *y) {
-    struct Node *x = y->left;
-    struct Node *T2 = x->right;
-
-    // Perform rotation
-    x->right = y;
-    y->left = T2;
-
-    // Update heights
-    y->height = max(height(y->left), height(y->right)) + 1;
-    x->height = max(height(x->left), height(x->right)) + 1;
-
-    // Return new root
-    return x;
-}
-
-// Function to rotate a subtree rooted with x to the left
-struct Node *leftRotate(struct Node *x) {
-    struct Node *y = x->right;
-    struct Node *T2 = y->left;
-
-    // Perform rotation
-    y->left = x;
-    x->right = T2;
-
-    // Update heights
-    x->height = max(height(x->left), height(x->right)) + 1;
-    y->height = max(height(y->left), height(y->right)) + 1;
-
-    // Return new root
-    return y;
-}
-
 // Function to create a new AVL tree node
 struct Node *newNode(struct Driver data) {
     struct Node *node = (struct Node *)malloc(sizeof(struct Node));
@@ -84,7 +43,6 @@ struct Node *newNode(struct Driver data) {
     node->left = NULL;
     node->right = NULL;
     node->height = 1; // New node is initially at height 1
-    node->unique_routes = 1; // New driver has taken one route by default
     return node;
 }
 
@@ -94,41 +52,19 @@ struct Node *insert(struct Node *node, struct Driver data) {
     if (node == NULL)
         return newNode(data);
 
-    if (strcmp(data.name, node->data.name) < 0)
+    int compareResult = strcmp(data.name, node->data.name);
+    if (compareResult < 0)
         node->left = insert(node->left, data);
-    else if (strcmp(data.name, node->data.name) > 0)
+    else if (compareResult > 0)
         node->right = insert(node->right, data);
     else {
         // Duplicate driver name, increment the unique_routes count
-        node->unique_routes++;
+        node->data.unique_routes++;
         return node;
     }
 
     // Update height of current node
     node->height = 1 + max(height(node->left), height(node->right));
-
-    // Get the balance factor to check whether this node became unbalanced
-    int balance = getBalance(node);
-
-    // Left Left Case
-    if (balance > 1 && strcmp(data.name, node->left->data.name) < 0)
-        return rightRotate(node);
-
-    // Right Right Case
-    if (balance < -1 && strcmp(data.name, node->right->data.name) > 0)
-        return leftRotate(node);
-
-    // Left Right Case
-    if (balance > 1 && strcmp(data.name, node->left->data.name) > 0) {
-        node->left = leftRotate(node->left);
-        return rightRotate(node);
-    }
-
-    // Right Left Case
-    if (balance < -1 && strcmp(data.name, node->right->data.name) < 0) {
-        node->right = rightRotate(node->right);
-        return leftRotate(node);
-    }
 
     return node;
 }
@@ -137,8 +73,22 @@ struct Node *insert(struct Node *node, struct Driver data) {
 void inOrder(struct Node *root) {
     if (root != NULL) {
         inOrder(root->left);
-        printf("%s %d\n", root->data.name, root->unique_routes);
+        printf("%s %s %d\n", root->data.name, root->data.route_id, root->data.unique_routes);
         inOrder(root->right);
+    }
+}
+
+// Function to print the top 10 drivers with the highest unique route counts
+void printTop10(struct Node *root, int *count) {
+    if (root != NULL && *count < 10) {
+        printTop10(root->right, count);
+
+        if (*count < 10) {
+            printf("%s %s %d\n", root->data.name, root->data.route_id, root->data.unique_routes);
+            (*count)++;
+        }
+
+        printTop10(root->left, count);
     }
 }
 
@@ -181,14 +131,19 @@ int main(int argc, char *argv[]) {
             strncpy(newDriver.name, name, sizeof(newDriver.name) - 1);
             newDriver.name[sizeof(newDriver.name) - 1] = '\0';
 
+            newDriver.unique_routes = 1; // Initialize the unique_routes count
             root = insert(root, newDriver);
         }
     }
 
     fclose(file);
 
-    printf("Driver names and the count of unique routes:\n");
+    printf("Driver names, route_id, and the count of unique routes:\n");
     inOrder(root);
+
+    printf("\nTop 10 drivers with the highest unique route counts:\n");
+    int count = 0;
+    printTop10(root, &count);
 
     freeAVL(root);
 
