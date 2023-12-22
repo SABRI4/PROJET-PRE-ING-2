dd70ff3bc433b4127e12160c411d7ee16ba83160
SABRI4 <131693312+SABRI4@users.noreply.github.com>
2023-12-22 10:35:59 +0100

    Update option1.sh

--- option1.sh
+++ option1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#répertoire home
 home="/home"
 
 if [ $# -eq 0 ]; then
@@ -10,15 +9,44 @@ fi
 
 csv_file="$1"
 
-cut -d ';' -f 1,6 "$csv_file" | sort | uniq -c | sort -nr > "$home/younes/Info/conducteurstrajets.txt"
-
-top_conducteurs=$(head -n 10 "$home/younes/Info/conducteurstrajets.txt")
+# Process CSV file and get the top 10 drivers
+top_conducteurs=$(cut -d ';' -f 1,6 "$csv_file" | sort | uniq -c | sort -nr | head -n 10)
 
 echo "Top 10 des conducteurs avec le plus de trajets différents:"
 echo "$top_conducteurs" | while read count name route_id; do
     printf "%-6s %-20s %-10s\n" "$count" "$name" "$route_id"
 done
 
-rm "$home/younes/Info/conducteurstrajets.txt"
+# Save data to a file for Gnuplot
+echo "$top_conducteurs" | awk '{print $3, $1}' > "$home/younes/Info/conducteurstrajets.dat"
+
+# Generate Gnuplot script
+gnuplot_script="$home/younes/Info/plot_script.gnu"
+cat > "$gnuplot_script" << EOL
+set terminal png
+set output 'conducteurstrajets_plot.png'
+set title 'Top 10 Conducteurs avec le Plus de Trajets Différents'
+set xlabel 'Nombre de Trajets'
+set ylabel 'Conducteurs'
+set style fill solid
+set boxwidth 0.42
+set ytics nomirror
+set yrange [0:60]
+set xtics rotate by -45
+plot "$home/younes/Info/conducteurstrajets.dat" using 2:xtic(1) with boxes title 'Nombre de Trajets'
+EOL
+
+# Run Gnuplot
+gnuplot "$gnuplot_script"
+
+# Open the PNG file
+eog "$home/younes/Info/conducteurstrajets_plot.png" 
+
+
+# Display completion message
+echo "Graphique créé avec succès : conducteurstrajets_plot.png"
+
+# Clean up temporary files
+rm "$home/younes/Info/conducteurstrajets.dat" "$gnuplot_script"
 
 exit 0
