568922d2bb72cb56c9f811d421acdbc200e5b202
SABRI4 <131693312+SABRI4@users.noreply.github.com>
2024-01-01 00:50:41 +0100

    Update option2.sh

--- option2.sh
+++ option2.sh
@@ -13,14 +13,35 @@ csv_file="$1"
 
 # Extraction des conducteurs et de la distance totale parcourue
 awk -F ';' '{ sum[$6] += $5 } END { for (i in sum) print sum[i], i }' "$csv_file" | \
-    sort -nr | head -n 10 > "$home/younes/Info/conducteur_distance.txt"
+    sort -nr | head -n 10 > "$home/younes/Info/conducteur_distance.dat"
+
+
 
 # Afficher le contenu du fichier temporaire
-echo "Contenu du fichier conducteur_distance.txt :"
-cat "$home/younes/Info/conducteur_distance.txt"
+echo "Contenu du fichier conducteur_distance.dat :"
+cat "$home/younes/Info/conducteur_distance.dat"
+
+gnuplot -persist <<PLOT
+
+set terminal png
+set output 'option2graph.png'
+set ylabel 'DISTANCE (Km)'  # This now becomes the categorical axis
+set xlabel 'DRIVERS NAMES'  # This now becomes the numerical axis
+set title 'Option -d2 : Distance = f(Driver)'
+set style data histograms
+set style histogram rowstacked  # This will stack the bars in one row
+set style fill solid border -1
+set boxwidth 0.65 relative
+set grid ytics
+set xtics rotate by -45
+unset yrange  # This allows Gnuplot to auto-scale the y-axis
+set datafile separator " "
+plot "$home/younes/Info/conducteur_distance.dat" using 1:xtic(sprintf("%s %s", stringcolumn(2), stringcolumn(3))) notitle lc rgb "purple"
+
+PLOT
 
 
-# Nettoyer les fichiers temporaires
-rm "$home/younes/Info/conducteur_distance.txt"
+
+
 
 exit 0
