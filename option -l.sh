7db1c10bbee75e0ee2b3a2bb39999a442987438b
SABRI4 <131693312+SABRI4@users.noreply.github.com>
2024-01-02 18:33:57 +0100

    Update option -l.sh

--- option -l.sh
+++ option -l.sh
@@ -1,44 +1,27 @@
 #!/bin/bash
 
-# Vérifier si le fichier CSV est fourni en argument
+# Définir le répertoire de base où se trouve le fichier CSV
+home="/home/younes/Info"
 
-
+# Vérifier si le nom du fichier CSV est fourni en argument
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 <csv_file>"
+    echo "Usage: $0 <csv_filename>"
     exit 1
 fi
-exec_timestart_l=$(date +%s.%N)
-csv_file="$1"
 
-# Extraction des ID et de la distance totale parcourue
-awk -F ';' '{ sum[$1] += $5 } END { for (i in sum) print sum[i], i }' "$csv_file" | \
-    sort -nr | head -n 10 > "ID_Trajet_distance.dat" // pb avc tri : tri doit etre croissant sur colonne n°2 (Route ID) et sort -n -k2 tri incorrectement
+# Chemin complet du fichier CSV
+csv_file="${home}/${1}"
 
-# Afficher le contenu du fichier temporaire
-echo "Contenu du fichier ID_Trajet_distance.dat :"
-echo "Distance  |  ID_Trajet "
-cat "ID_Trajet_distance.dat"
+# Calculer la distance totale de chaque trajet et trier les résultats
+awk -F';' '
+    NR > 1 { distance[$1] += $5 }
+    END {
+        for (route in distance) {
+            print distance[route], route
+        }
+    }
+' "$csv_file" | sort -nrk1,1 | head -n 10 | sort -nk2,2 > "${home}/top_trajets.dat"
 
-exec_timeend_l=$(date +%s.%N)
-exec_timetotal_l=$(echo "$exec_timeend_l - $exec_timestart_l" | bc)
-
-echo "Temps d'exécution total du script : $exec_timetotal_l secondes"
-
-gnuplot -persist <<-PLOT
-
-set terminal png
-set output 'Trajet_Distance.png'
-set ylabel 'DISTANCE (km)'
-set xlabel 'ROUTE ID'
-set title 'Top 10 trajets les plus long'
-set style data histograms
-set style fill solid 1.0 border -1
-set boxwidth 1
-set grid ytics
-unset xrange
-set datafile separator " "
-plot 'ID_Trajet_distance.dat' using 1:xtic(sprintf("%s %s", stringcolumn(2), stringcolumn(3))) notitle lc rgb "green" // pb dans disposition et manipulatiuon histo
-
-PLOT
-
-exit 0
+# Afficher les 10 trajets les plus longs triés par identifiant de trajet
+echo "Les 10 trajets les plus longs :"
+cat "${home}/top_trajets.dat"
