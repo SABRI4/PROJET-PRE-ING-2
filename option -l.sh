ef51d9535742cce67cd0c4ea6618230b6aa14334
Pachax17 <131616395+Pachax17@users.noreply.github.com>
2024-01-10 14:18:15 +0100

    Update option -l.sh

--- option -l.sh
+++ option -l.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Définir le répertoire de base où se trouve le fichier CSV
-home="/home/younes/Info"
-
 # Vérifier si le nom du fichier CSV est fourni en argument
 if [ $# -eq 0 ]; then
     echo "Usage: $0 <csv_filename>"
@@ -10,19 +7,41 @@ if [ $# -eq 0 ]; then
 fi
 
 # Chemin complet du fichier CSV
-csv_file="${home}/${1}"
+exec_timestart_l=$(date +%s.%N)
+csv_file="${1}"
 
 # Calculer la distance totale de chaque trajet et trier les résultats
-awk -F';' '
+awk -b -F';' '
     NR > 1 { distance[$1] += $5 }
     END {
         for (route in distance) {
             print distance[route], route
         }
     }
-' "$csv_file" | sort -nrk1,1 | head -n 10 | sort -nk2,2 > "${home}/top_trajets.dat"
+' "$csv_file" | sort -nrk1,1 | head -n 10 | sort -nk2,2 > "temp/top_trajets.dat"
 
 # Afficher les 10 trajets les plus longs triés par identifiant de trajet
 echo "Les 10 trajets les plus longs :"
-cat "${home}/top_trajets.dat"
-xticlabels
+cat "temp/top_trajets.dat"
+
+exec_timeend_l=$(date +%s.%N)
+exec_timetotal_l=$(echo "$exec_timeend_l - $exec_timestart_l" | bc)
+
+echo "Temps d'exécution total du script : $exec_timetotal_l secondes"
+
+gnuplot -persist <<-PLOT
+
+set terminal png
+set output 'Image/Trajet_Distance.png'
+set ylabel 'DISTANCE (km)'
+set xlabel 'ROUTE ID'
+set title 'Top 10 trajets les plus long'
+set style data histograms
+set style fill solid 1.0 border -1
+set boxwidth 1
+set grid ytics
+set yrange [0:*]
+set datafile separator " "
+plot 'temp/top_trajets.dat' using 1:xtic(2) notitle lc rgb "green"
+
+PLOT
