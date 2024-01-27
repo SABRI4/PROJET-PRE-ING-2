1e1631b197c7949853c37017784bfaf760169576
SABRI4 <131693312+SABRI4@users.noreply.github.com>
2024-01-27 18:19:13 +0100

    Update option2.sh

--- option2.sh
+++ option2.sh
@@ -15,29 +15,31 @@ csv_file="$1"
 awk -b -F ';' '{ sum[$6] += $5 } END { for (i in sum) print sum[i], i }' "$csv_file" | \
     sort -nr | head -n 10 > "$home/younes/Info/conducteur_distance.dat"
 
-
-
-# Afficher le contenu du fichier temporaire
-echo "Contenu du fichier conducteur_distance.dat :"
-cat "$home/younes/Info/conducteur_distance.dat"
-
 gnuplot -persist <<PLOT
 
-set terminal png
+set terminal png size 700,1000
 set output 'option2graph.png'
-set ylabel 'DISTANCE (Km)'  # This now becomes the categorical axis
-set xlabel 'DRIVERS NAMES'  # This now becomes the numerical axis
-set title 'Option -d2 : Distance = f(Driver)'
+set ylabel 'OPTION -D2'
+set y2label 'DISTANCE (KM)' offset -3,0
+set xlabel 'DISTANCE (KM)' rotate by  180 offset character 0, -11, 0
+set ytics rotate by 90 
+set xtics font "Arial, 11" 
+set xtic rotate by 90 scale 0 offset character 0, -11, 0
 set style data histograms
-set style histogram rowstacked  # This will stack the bars in one row
-set style fill solid border -1
-set boxwidth 0.65 relative
+set style fill solid 0.5 border -1
+set boxwidth 1
 set grid ytics
-set xtics rotate by -45
-unset yrange  # This allows Gnuplot to auto-scale the y-axis
+unset yrange
+set size 0.6, 1
 set datafile separator " "
+set lmargin 10
+set bmargin 15
+set rmargin 0.5
+set tmargin 5
 plot "$home/younes/Info/conducteur_distance.dat" using 1:xtic(sprintf("%s %s", stringcolumn(2), stringcolumn(3))) notitle lc rgb "purple"
 
 PLOT
 
+
+convert -rotate 90 option2graph.png option2graph.png
 exit 0
