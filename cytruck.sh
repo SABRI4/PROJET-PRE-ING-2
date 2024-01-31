6643d6c57765ea048a462bf1e0ec8662111113c2
Pachax17 <131616395+Pachax17@users.noreply.github.com>
2024-01-31 19:42:36 +0100

    Update cytruck.sh

--- cytruck.sh
+++ cytruck.sh
@@ -2,20 +2,32 @@
 
 #accorde acces exec à tout les script
 chmod +x cytruck.sh
-chmod +x optionLV4.sh
+chmod +x optionL.sh
 chmod +x option1.sh
 chmod +x option2.sh
 chmod +x options.sh
 chmod +x optiont.sh
+
 # Fonction pour afficher l'aide
 print_help() {
-    echo "Usage: $0 -d1 | -d2 | -t | -s | -l FILE"
+    echo "Usage: $0 -d1 | -d2 | -t | -s | -l <FILE>"
     echo "Options:"
     echo "  -d1      Appelle le script optiond1.sh"
     echo "  -d2      Appelle le script optiond2.sh"
     echo "  -t       Appelle le script optiont.sh"
     echo "  -s       Appelle le script options.sh"
     echo "  -l       Appelle le script optionLV4.sh"
+}
+
+
+# Vérification de la présence du dossier temp
+check_temp_folder() {
+    if [ ! -e "temp" ]; then
+        echo "Création du dossier temp"
+        mkdir temp
+    else
+        echo "Le dossier temp existe déjà."
+    fi
 }
 
 # Vérification de la présence du dossier data
@@ -28,35 +40,25 @@ check_data_folder() {
     fi
 }
 
-# Vérification de la présence du dossier temp
-check_temp_folder() {
-    if [ ! -e "temp" ]; then
-        echo "Création du dossier temp"
-        mkdir temp
-    else
-        echo "Le dossier temp existe déjà."
-    fi
-}
-
 # Vérification de la présence du dossier images
 check_images_folder() {
     if [ ! -e "images" ]; then
         echo "Création du dossier images"
         mkdir images
     else
-          echo "Le dossier images existe déjà."
+        echo "Le dossier images existe déjà."
     fi
 }
 
 # Vérification du nombre d'arguments
 if [ "$#" -ne 2 ]; then
-    echo "Erreur: Le fichier CSV en argument est manquant."
+    echo "Erreur: il y a trop d'argumentsou pas assez"
     print_help
     exit 1
 fi
 
 # Vérification de l'existence du fichier CSV
-csv_file="$2"
+csv_file="$1"
 
 if [ ! -f "$csv_file" ]; then
     echo "Erreur: Le fichier CSV '$csv_file' n'existe pas."
@@ -65,21 +67,21 @@ if [ ! -f "$csv_file" ]; then
 fi
 
 # Vérification des options
-case "$1" in
+case "$2" in
     -d1)
-        ./option1.sh $2
+        ./option1.sh $1
         ;;
     -d2)
-        ./option2.sh $2
+        ./option2.sh $1
         ;;
     -t)
-        ./optiont.sh optt.c $2
+        ./optiont.sh optt.c $1
         ;;
     -s)
-        ./options.sh opts.c $2
+        ./options.sh opts.c $1
         ;;
     -l)
-        ./optionLV4.sh $2
+        ./optionL.sh $1
         ;;
     *)
         echo "Erreur: Option invalide."
@@ -89,7 +91,7 @@ case "$1" in
 esac
 
 # Vérification des dossiers temp et images
-check_temp_folder
+check_data_folder
 check_images_folder
 
 echo "Le script a été exécuté avec succès."
