c7c31f49efe0e7f1e4d0bafeac626d4208be036c
Pachax17 <131616395+Pachax17@users.noreply.github.com>
2024-01-31 19:49:57 +0100

    Update option -l.sh

--- option -l.sh
+++ option -l.sh
@@ -6,7 +6,7 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Chemin complet du fichier CSV
+#depart chrono / Chemin complet du fichier CSV
 exec_timestart_l=$(date +%s.%N)
 csv_file="${1}"
 
@@ -18,31 +18,35 @@ awk -b -F';' '
             print distance[route], route
         }
     }
-' "$csv_file" | sort -nrk1,1 | head -n 10 | sort -nk2,2 > "temp/top_trajets.dat"
+' "$csv_file" | sort -nrk1,1 | head -n 10 | sort -nk2,2nr > "data/top_trajets.dat"
 
 # Afficher les 10 trajets les plus longs triés par identifiant de trajet
 echo "Les 10 trajets les plus longs :"
-cat "temp/top_trajets.dat"
+echo "Distance|ID_Trajet "
+cat "data/top_trajets.dat"
 
 exec_timeend_l=$(date +%s.%N)
 exec_timetotal_l=$(echo "$exec_timeend_l - $exec_timestart_l" | bc)
 
 echo "Temps d'exécution total du script : $exec_timetotal_l secondes"
 
+
 gnuplot -persist <<-PLOT
 
 set terminal png
-set output 'Image/Trajet_Distance.png'
+set output 'images/Trajet_Distance.png'
 set ylabel 'DISTANCE (km)'
 set xlabel 'ROUTE ID'
 set title 'Top 10 trajets les plus long'
 set style data histograms
 set style fill solid 1.0 border -1
 set boxwidth 1
-set grid ytics
 set xtics rotate by -45
+set grid ytics
 set yrange [0:*]
 set datafile separator " "
-plot 'temp/top_trajets.dat' using 1:xtic(2) notitle lc rgb "green"
+plot 'data/top_trajets.dat' using 1:xtic(2) notitle lc rgb "green"
 
 PLOT
+
+exit 0
