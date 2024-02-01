a5978eb9e89d11bf9b4dbb6f6e319369e1817303
naelbe <128997008+naelbe@users.noreply.github.com>
2024-02-01 16:05:09 +0100

    Update opts.c

--- opts.c
+++ opts.c
@@ -1,35 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
+#include "opts.h"
+
 
 #define MACRO_LOWSAMPLE 0 // en cas de petit nb de row le max = le min car pas de plusieurs etap
-#define MACRO_PRINTROW 0 //permet de print les ----ROW---- ou non
 
-/*
-free traj / avl / avl2
-commentaires
-*/
-
-typedef struct trajetInfo {
-int id;
-int etap;
-float tot;
-float max;
-float min;
-float moy;
-}inf;
-
-typedef struct avlNode {
-float key;
-struct trajetInfo traj;
-int height;
-struct avlNode *fg;
-struct avlNode *fd;
-}node;
 
 int counter;
 
+
+//fonction qui retourne un noeud de l'avl
 node *newNode(inf *dist) {
 node *new = malloc(sizeof(node));
 if(new != NULL) {
@@ -40,6 +18,8 @@ new->fd = NULL;
 return new;
 }
 }
+
+//fonction qui retourne une structure contenant les stats nécessaire au traitement de l'option s
 inf *newInfo(int key, float dist) {
 inf *new = malloc(sizeof(inf));
 if(new != NULL) {
@@ -102,8 +82,9 @@ y->height = 1 + max(getH(y->fg), getH(y->fd));
 return y;
 }
 
+
+//fonction d'insertion dans le premier avl : trie les trajets arbitrairement par leur ID, si deux ID sont identiques, on additionne les valeurs
 node *insert2(node *avl, inf *newInf) {
-//printf("INSERT2 CALL WITH KEY %d | DIST %f\n", newInf->id, newInf->max);
 if(avl == NULL){
 return newNode(newInf);
 }
@@ -116,10 +97,8 @@ else if(key < avl->traj.id) {
 avl->fg = insert2(avl->fg, newInf);
 }
 else if(key == avl->traj.id){ 
-//printf("EGALITE DE TRAJETID  %d \n", key);
 if(newInf->max > avl->traj.max) {avl->traj.max = newInf->max;}
 if(newInf->min < avl->traj.min) {avl->traj.min = newInf->min;}
-//printf("ADDED %f to MOY %f\n", newInf->min, avl->traj.moy);
 avl->traj.moy += newInf->min;
 avl->traj.etap++;
 }
@@ -157,7 +136,9 @@ return avl;
 
 }
 
-node * proCsv(node *avl, char *csv, int row) {  // pro csv a adapter a la logique de -s
+
+//fonction de lécture du fichier CSV
+node * proCsv(node *avl, char *csv, int row) { 
 FILE *file = fopen(csv, "r");
 if( file == NULL) {printf("file NULL\n"); exit(1);}
 
@@ -170,9 +151,6 @@ int fline = 1;
 while(fgets(buff, sizeof(buff), file) != NULL && ct < row) {
 char *token = strtok(buff, ";");
 col = 1;
-if(MACRO_PRINTROW){
-printf("-------------------------------------ROW %d----------------------\n", ct);// A DELETE
-}
 if(fline) {
 fline = 0;
 ct++;
@@ -180,12 +158,10 @@ continue;
 }
 
 while(token != NULL && col <= 6) {
-//printf("inside while 1 || COL = %d\n", col);
 if(col == 1) {
 trajet = atoi(token);
 }
 else if(col == 5) {
-//printf("proCsv : call hashInsert with %d & %f\n", trajet, atof(token));
 inf *newInf = newInfo(trajet, atof(token));
 avl = insert2(avl, newInf);
 }
@@ -197,22 +173,18 @@ ct++;
 fclose(file);
 return avl;
 }
+
+//insértion dans le second avl en fonction de "tot" qui est max - min (logique demandée dans la déscription du projet)
 node *insert(node *avl, inf *dist) {
-//printf("INSERT CALL WITH KEY %f | MAX %f(already good max)\n", dist->tot, dist->max);
 if(avl == NULL){
 return newNode(dist);
 }
 float key = dist->tot;
-//printf("CITY KEY %d\n", key);
-//printf("AVLINSERT : %d | %f\n", dist->id, key);
 if(key > avl->traj.tot) {
 avl->fd = insert(avl->fd, dist);
 }
 else if(key < avl->traj.tot) {
 avl->fg = insert(avl->fg, dist);
-}
-else if(key == avl->traj.tot){ //meme nb d'occ mais ville forcement differentes car doublons rassemlbés dans htable.
-//printf("DEUX DISTANCES TOTALES EGALES (CAS PARTICULIER) = %f\n", key); // cas impossible /tres rare ?
 }
 
 int balance = getB(avl);
@@ -248,13 +220,13 @@ return avl;
 
 }
 
+//fonction qui parcours le premier avl et insert la structure "traj" (trajet) dans le second avl
 node *parseTable(node **avl, node *avl2) {
 if(avl2 == NULL) {
 return NULL;
 }
 parseTable(avl, avl2->fg);
 
-//printf("PARSETABLE AVL1 CALL %f\n", avl2->traj.tot);
 inf *new = &avl2->traj;
 *avl = insert(*avl, new);
 free(avl2);
@@ -264,6 +236,7 @@ parseTable(avl, avl2->fd);
 return *avl;
 }
 
+//fonction qui permet de calculer la moyenne et le total, car ces deux valeurs ne sont calculable qu'après avoir obtenu l'ensemble des données
 node *adjust(node *avl) {
 if(avl == NULL) {
 return NULL;
@@ -275,17 +248,16 @@ if(MACRO_LOWSAMPLE == 0) {
 avl->traj.tot = avl->traj.max - avl->traj.min;
 }
 else {
-avl->traj.tot = avl->traj.max; //pr les petit sample, max = min par def de newInf donc fausse insert1
+avl->traj.tot = avl->traj.max; 
 }
-//printf("ADJUSTED MOY TO %f\n", avl->traj.moy);
-//printf("ADJUSTED TOT TO %f\n", avl->traj.tot);
 
 adjust(avl->fd);
 
 return avl;
 }
 
-void parcours(node *avl) { // inverser l'ordre (affichage du terminal trop petit donc + grand en dernier)
+//print les résultats selon la quantité demandée et au format adapté à GNUPLOT
+void parcours(node *avl) { 
 if(avl == NULL) {
 return;
 }
@@ -293,7 +265,6 @@ parcours(avl->fd);
 
 if(counter < 50) {
 printf("%d |ID|%d;|TOTAL|%f;|MOYENNE|%f;|ETAPES|%d|MAX|%f;|MIN|%f\n", counter, avl->traj.id, avl->traj.tot, avl->traj.moy, avl->traj.etap, avl->traj.max, avl->traj.min);
-//printf("ID %d | TOTAL %f | MOYENNE %f | MAX %f | MIN %f | ETAPE %d | BAL %d\n", avl->traj.id, avl->traj.tot, avl->traj.moy, avl->traj.max, avl->traj.min, avl->traj.etap, getB(avl));
 free(avl->traj);
 free(avl);
 counter++;
@@ -308,7 +279,6 @@ parcours(avl->fg);
 
 int main(int argc, char *argv[]) {
 
-//clock_t start_time = clock();
 int row = 0;
 char csv[50];
 
@@ -326,11 +296,6 @@ strcpy(csv, argv[2]);
 avl2 = proCsv(avl2, csv, row);
 avl2 = adjust(avl2); // crée les moyennes + crée les totaux
 avl = parseTable(&avl, avl2);
-//clock_t time1 = clock();
 counter = 0;
 parcours(avl);
-//clock_t time2 = clock();
-//double elap1 = ((double)(time2 - start_time))/CLOCKS_PER_SEC;
-//double elap2 = ((double)(time1 - start_time))/CLOCKS_PER_SEC;
-//printf("EXECTIME : TOTAL = %f\nEXECTIME : REAL = %f\n", elap1, elap2);
 }
