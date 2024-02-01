1a4ccbaa4b69d82409f2a34967e2bae43e910373
SABRI4 <131693312+SABRI4@users.noreply.github.com>
2024-02-01 20:41:59 +0100

    Update option-s.sh

--- option-s.sh
+++ option-s.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#chrono depart
-exec_timestart_l=$(date +%s.%N)
-
 # Vérification de la présence de deux arguments
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <C PROGRAM> <csv_file>"
@@ -11,23 +8,16 @@ fi
 
 executable=$1
 csv_file=$2
-lines=$(wc -l $2)
+
+gcc $1 -o code2
 
 # Exécution de l'exécutable pour traiter le fichier CSV et création de temp.dat
-if [ ! -e progc/opts.c ] ;then
-echo "Erreur, Fichier inexistant"
-exit 2
-elif [ -e progc/opts.c ] ;then
-cd progc
-./opts $lines $csv_file > ../temp/temp.dat
-cd ..
-fi
-
+./code2 6000000 $csv_file > temp.dat
 
 # Utilisation de awk pour transformer les données en un format attendu par Gnuplot
-awk -b -F '|' '{print $3, $7, $11, $13 }' temp/temp.dat > ../data/option-s.dat
+awk -F '|' '{print $3, $7, $11, $13 }' temp.dat > option-s.dat
 # Suppression du fichier temporaire
-rm temp/temp.dat
+rm temp.dat
 
 # Vérification de l'existence de données dans option-s.dat
 if [ ! -s option-s.dat ]; then
@@ -35,16 +25,10 @@ if [ ! -s option-s.dat ]; then
     exit 2
 fi
 
-#fin chrono
-exec_timeend_l=$(date +%s.%N)
-exec_timetotal_l=$(echo "$exec_timeend_l - $exec_timestart_l" | bc)
-
-echo "Temps d'exécution total du script : $exec_timetotal_l secondes"
-
 # Script Gnuplot
 gnuplot -persist <<-EOF
     set terminal png size 1400, 600
-    set output 'image/option-s_graph.png'
+    set output 'option-s_graph.png'
     set ylabel 'Distance (km)'
     set xlabel 'ROUTE ID'
     set title 'Option -s: Distance f(Route)'
@@ -54,9 +38,11 @@ gnuplot -persist <<-EOF
     set yrange [0:*]
     unset xrange
     set datafile separator ";"
-    plot 'data/option-s.dat' using 2:xtic(1) title 'Distance average (Km)' with lines, \
-         'data/option-s.dat' using 3:xtic(1) title 'Distances Max (Km)' with lines, \
-         'data/option-s.dat' using 4:xtic(1) title 'Distances Min (Km)' with lines
+    plot 'option-s.dat' using 2:xtic(1) title 'Distance average (Km)' with lines, \
+         'option-s.dat' using 3:xtic(1) title 'Distances Max (Km)' with lines, \
+         'option-s.dat' using 4:xtic(1) title 'Distances Min (Km)' with lines
 EOF
 
 exit 0
+
+
